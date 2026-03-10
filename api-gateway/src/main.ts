17f99296d6998cd2ab65b50160661da267770896
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2026-03-10 09:57:50 +0530

    first commit

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -5,6 +5,13 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   app.use(
     '/library/books',
     createProxyMiddleware({
