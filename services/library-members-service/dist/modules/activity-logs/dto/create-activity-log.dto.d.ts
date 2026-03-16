0dadf14af226ab83e938bc4bdd49e9735414c547
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2026-03-16 19:08:31 +0530

    first commit

--- services/library-members-service/dist/modules/activity-logs/dto/create-activity-log.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateActivityLogDto {
-    action: string;
-    bookId?: string;
-    memberId?: string;
-    userId?: string;
-    description: string;
-    performedBy: string;
-}
