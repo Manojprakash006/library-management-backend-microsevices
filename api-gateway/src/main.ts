1de10c8b35f1b7d86e05891f0c9b1b7d5a9e1faf
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2026-03-30 10:51:49 +0530

    first commit

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization, Pragma, Cache-Control',
